bfc5e0479257a63c68dd52a5b426ea60676fc862
Steve King, Jr <stevekx86@gmail.com>
2015-06-23 13:37:42 -0700

    Headers have name and content. Requests use Sets of Headers.

--- src/com/stevex86/napper/request/Request.java
+++ src/com/stevex86/napper/request/Request.java
@@ -1,5 +1,13 @@
 package com.stevex86.napper.request;
 
+import com.stevex86.napper.request.header.Header;
+import com.stevex86.napper.request.method.RequestMethod;
+
+import java.util.Set;
+
 public class Request {
+
+    private RequestMethod requestMethod;
+    private Set<Header> headers;
 
 }
--- src/com/stevex86/napper/request/header/Header.java
+++ src/com/stevex86/napper/request/header/Header.java
@@ -2,4 +2,20 @@ package com.stevex86.napper.request.header;
 
 public class Header {
 
+    private String headerName;
+    private String headerContent;
+
+    public Header(String headerName, String headerContent) {
+        this.headerName = headerName;
+        this.headerContent = headerContent;
+    }
+
+    public String getHeaderName() {
+        return headerName;
+    }
+
+    public String getHeaderContent() {
+        return headerContent;
+    }
+
 }
