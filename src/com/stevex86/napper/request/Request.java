5bb5fa977a62649fcbbd7793cb7368e3d7f5e81e
Steve King, Jr <stevekx86@gmail.com>
2015-06-23 16:42:57 -0700

    Adding a few more contained members to the Request class.

--- src/com/stevex86/napper/request/Request.java
+++ src/com/stevex86/napper/request/Request.java
@@ -1,7 +1,8 @@
 package com.stevex86.napper.request;
 
-import com.stevex86.napper.request.header.Header;
-import com.stevex86.napper.request.method.RequestMethod;
+import com.stevex86.napper.http.elements.content.Content;
+import com.stevex86.napper.http.elements.header.Header;
+import com.stevex86.napper.http.elements.method.RequestMethod;
 
 import java.util.Set;
 
@@ -9,5 +10,19 @@ public class Request {
 
     private RequestMethod requestMethod;
     private Set<Header> headers;
+    private Content content;
+
+    public Request(RequestMethod requestMethod) {
+        this.requestMethod = requestMethod;
+    }
+
+
+    public void addHeader(Header header) {
+        headers.add(header);
+    }
+
+    public void addHeaders(Set<Header> headers) {
+        this.headers.addAll(headers);
+    }
 
 }
