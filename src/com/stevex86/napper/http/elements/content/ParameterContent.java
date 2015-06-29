14a2703422081c587b19ed1eb80e9eceec831b86
Steve King, Jr <stevekx86@gmail.com>
2015-06-29 08:44:17 -0700

    Instatiate hashmap.

--- src/com/stevex86/napper/http/elements/content/ParameterContent.java
+++ src/com/stevex86/napper/http/elements/content/ParameterContent.java
@@ -1,5 +1,6 @@
 package com.stevex86.napper.http.elements.content;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ParameterContent implements Content {
@@ -7,7 +8,7 @@ public class ParameterContent implements Content {
     private Map<String, ?> content;
 
     public ParameterContent() {
-
+        content = new HashMap<String, Object>();
     }
 
     @Override
