6d857797dca6708dfb53ca548a64d46d564848f9
Steve King, Jr <stevekx86@gmail.com>
2015-06-29 09:16:24 -0700

    ParameterQueryContent adds and supports entries now.

--- src/com/stevex86/napper/http/elements/content/ParameterQueryContent.java
+++ src/com/stevex86/napper/http/elements/content/ParameterQueryContent.java
@@ -1,19 +1,56 @@
 package com.stevex86.napper.http.elements.content;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ParameterQueryContent implements QueryContent {
 
-    private Map<String, ?> content;
+    private Map<String, Object> content;
+    private String builtQueryString;
 
     public ParameterQueryContent() {
         content = new HashMap<String, Object>();
     }
 
+    public void addEntry(String key, String value) {
+        if (key.equals("") || value.equals("")) {
+            throw new IllegalArgumentException("Key and value must both be non-empty strings.");
+        }
+        else {
+            content.put(key, value);
+            if (builtQueryString.equals("")) {
+                try {
+                    builtQueryString += URLEncoder.encode(key, "UTF-8");
+                    builtQueryString += "=";
+                    builtQueryString += URLEncoder.encode(value, "UTF-8");
+                }
+                catch (UnsupportedEncodingException e) {
+                    throw new IllegalStateException("System does not support UTF-8 encoding!");
+                }
+            }
+            else {
+                try {
+                    builtQueryString += "&";
+                    builtQueryString += URLEncoder.encode(key, "UTF-8");
+                    builtQueryString += "=";
+                    builtQueryString += URLEncoder.encode(value, "UTF-8");
+                }
+                catch (UnsupportedEncodingException e) {
+                    throw new IllegalStateException("System does not support UTF-8 encoding!");
+                }
+            }
+        }
+    }
+
+    public void addEntry(String key, Object value) {
+        addEntry(key, value.toString());
+    }
+
     @Override
     public String getQueryString() {
-        return null;
+        return builtQueryString;
     }
 
 }
