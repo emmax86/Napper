67984f37910c031ee812fd6bdcdbd3f5dc52d0d4
Steve King, Jr <stevekx86@gmail.com>
2015-07-13 22:40:20 -0700

    Added Route class.

---
+++ src/com/stevex86/napper/http/elements/route/Route.java
@@ -0,0 +1,34 @@
+package com.stevex86.napper.http.elements.route;
+
+public class Route {
+
+    private String route;
+    private String URL;
+    private int port;
+
+    public Route(String route) {
+        this.route = route;
+        String[] tokens = route.split(":");
+        if (tokens.length > 2 && !tokens[2].equals("")) {
+            this.port = Integer.parseInt(tokens[2]);
+            this.URL = tokens[0] + tokens[1];
+        }
+        else {
+            this.port = tokens[0].equalsIgnoreCase("https") ? 443 : 80;
+            this.URL = tokens[0] + tokens[1];
+        }
+    }
+
+    public boolean isSecure() {
+        return route.toLowerCase().startsWith("https");
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public int getPort() {
+        return port;
+    }
+    
+}
